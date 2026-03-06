20420bc46a3fdc50de702069bf06c7d4ee6fb760
Antonio Rodriguez <antoniorodriguez@Mac-mini-de-Antonio.local>
2026-03-05 20:58:11 -0600

    Create file to ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,27 @@
+# Ejercicio 1
+
+# Escribe un script que haga lo siguiente: dado un texto tamaño N, deberá decir las veces que ese texto aparece en un
+# párrafo de tamaño M.
+# Ejemplo:
+# Parrafo: “La logística Digital es un concepto que surge de la integración entre la logística tradicional y la era digital.
+# Con el auge del correo electrónico y las descargas digitales reemplazando productos físicos, podríamos estar
+# hablando de un golpe devastador para la industria de la logística, pero, de hecho, ha ocurrido algo muy diferente. El
+# sector de la logística ha introducido las innovaciones digitales.”
+# texto: “logística”
+# Salida: “4 ocurrencias encontradas”
+
+PARRAFO = """La logística Digital es un concepto que surge de la integración entre la logística tradicional y la era digital.
+Con el auge del correo electrónico y las descargas digitales reemplazando productos físicos, podríamos estar
+hablando de un golpe devastador para la industria de la logística, pero, de hecho, ha ocurrido algo muy diferente. El
+sector de la logística ha introducido las innovaciones digitales."""
+
+texto = "Digital"
+
+class Solution():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    
