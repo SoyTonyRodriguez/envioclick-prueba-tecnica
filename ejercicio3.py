3bac113a553e1dde782115d73ddadfa4c9dcf7a5
Antonio Rodriguez <antoniorodriguez@Mac-mini-de-Antonio.local>
2026-03-06 00:52:37 -0600

    Feat: Create ejercicio3 file

---
+++ ejercicio3.py
@@ -0,0 +1,12 @@
+# Ejercicio 3
+# Crear una representación básica de una hoja de excel, esta representación
+# debe contar con celda que se representan por filas y columnas, debe haber 6
+# funcionalidades basicas dentro de esta hoja
+#  - Insertar información en una celda
+#  - Actualizar información en una celda
+#  - Validar si una celda tiene información dentro
+#  - Mostrar un preview de la hoja entera
+#  - Dada una fila recuperar todos los elementos e imprimir la suma de todos
+#       los valores dados
+#  - Dada una columna recuperar todos los elementos e imprimir la suma de todos
+#       los valores dados
